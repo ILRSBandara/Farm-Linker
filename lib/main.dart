8dedc18e52519f545c3a486a8602148d0b992556
Saundarya Iddamalgoda <160006775+ILRSBandara@users.noreply.github.com>
2024-04-03 15:50:13 +0530

    Connected to the Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:farm_linker/signinpage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main(){
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -10,7 +18,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home:SignInPage(
+      
+      ),
     );
   }
 }
